d4d3dd81952177ccac5dd647f18332154cf08775
ydevansh-code <dy6.idk@gmail.com>
2026-07-31 09:10:57 +0530

    ✅ Palindrome Number | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 8.6 MB | 2026-07-31

---
+++ leetcode/easy/0009-palindrome-number/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        long long reversed = 0;
+        int temp = x;
+        while (temp > 0) {
+            reversed = reversed * 10 + temp % 10;
+            temp /= 10;
+        }
+        return reversed == x;
+        
+    }
+};
