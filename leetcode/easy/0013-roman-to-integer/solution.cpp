436278064fd6f28a82181d743eba920aa4d312e0
ydevansh-code <dy6.idk@gmail.com>
2026-07-31 09:11:07 +0530

    ✅ Roman to Integer | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 10.1 MB | 2026-07-31

---
+++ leetcode/easy/0013-roman-to-integer/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int getVal(char c) {
+        switch(c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+    int romanToInt(string s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length() && getVal(s[i]) < getVal(s[i + 1])) {
+                result -= getVal(s[i]);
+            } else {
+                result += getVal(s[i]);
+            }
+        }
+        return result;
+    }
+};
